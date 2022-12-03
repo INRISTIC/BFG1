54059dc837eb89d1928ab6d3bc2ec3a2930d77ea
матвей тепайкин <m_769_m@vk.com>
2022-12-03 21:22:01 +0700

    03_12_2022v4

--- src/components/Pages/Information/InfoElement/InfoElement.jsx
+++ src/components/Pages/Information/InfoElement/InfoElement.jsx
@@ -1,10 +1,22 @@
 import s from "./InfoElement.module.css";
+import { ReactComponent as Arrow } from "../../../../assets/images/arrow-bottom.svg";
+
+import { useState } from "react";
 
 const InfoElement = () => {
 
+  const [open, setOpen] = useState(false);
+  console.log(open);
   return (
-    <div className={s.infoElement}>
-      
+    <div className={open ? s.infoElement : s.infoElement + ' ' + s.hidden} onClick={() => setOpen(!open)}>
+      <div className={s.infoElement__header_block}>
+        <span className={s.infoElement__header_text}>Как попасть в ТОП 100</span>
+
+        <button className={open ? s.infoElement__header_btn + ' ' + s.infoElement__header_btn_active : s.infoElement__header_btn}>
+          <Arrow className={s.arrow} />
+        </button>
+      </div>
+
     </div>
   )
 }
