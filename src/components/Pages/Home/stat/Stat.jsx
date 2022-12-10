aba9043b989b29b5fa751cd8251ec17181bad191
матвей тепайкин <m_769_m@vk.com>
2022-12-10 10:05:00 +0600

    10_12_2022v9

--- src/components/Pages/Home/stat/Stat.jsx
+++ src/components/Pages/Home/stat/Stat.jsx
@@ -8,6 +8,8 @@ const Stat = () => {
   return (
     <div className={s.stat}>
       <div className={s.statMainBlock}>
+        <div className={s.statInfoBlockCircles}></div>
+
         <div className={s.statInfoBlockImgContainer}>
           <img src={statInfoBlockImg} alt="" />
         </div>
@@ -15,8 +17,37 @@ const Stat = () => {
         <button className={s.statInfoBlockBtn}>Перейти в бота</button>
       </div>
 
-      <div className={s.statInfoBlock}></div>
-      <div className={s.statInfoBlock}></div>
+      <div className={s.statInfoBlock}>
+        <div className={s.statInfoElement}>
+          <div className={s.statInfoElementTextBlock}>
+            <span>Онлайн</span>
+            <span>153</span>
+          </div>
+        </div>
+
+        <div className={s.statInfoElement}>
+          <div className={s.statInfoElementTextBlock}>
+            <span>Количество пользователей</span>
+            <span>35 312</span>
+          </div>
+        </div>
+      </div>
+
+      <div className={s.statInfoBlock}>
+        <div className={s.statInfoElement}>
+          <div className={s.statInfoElementTextBlock}>
+            <span>Количество сообщений</span>
+            <span>35 682 195</span>
+          </div>
+        </div>
+
+        <div className={s.statInfoElement}>
+          <div className={s.statInfoElementTextBlock}>
+            <span>Количество дней с основания</span>
+            <span>162</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
