a2359e1025ec9dd47f30c11e75e0d59651038792
матвей тепайкин <m_769_m@vk.com>
2022-12-10 10:36:22 +0600

    10_12_2022v10

--- src/components/Pages/Home/stat/Stat.jsx
+++ src/components/Pages/Home/stat/Stat.jsx
@@ -1,54 +1,100 @@
 import React from "react";
+import { useLayoutEffect, useState } from "react";
 
 import s from "./Stat.module.css";
 
 import statInfoBlockImg from "../../../../assets/images/statMainBlockImg.svg";
 
+
+function useWindowSize() {
+  const [size, setSize] = useState([0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+}
+
 const Stat = () => {
+  const [width] = useWindowSize();
+
   return (
     <div className={s.stat}>
-      <div className={s.statMainBlock}>
-        <div className={s.statInfoBlockCircles}></div>
+      {width > 830 ? (
+        <>
+          <div className={s.statMainBlock}>
+            <div className={s.statInfoBlockCircles}></div>
 
-        <div className={s.statInfoBlockImgContainer}>
-          <img src={statInfoBlockImg} alt="" />
-        </div>
+            <div className={s.statInfoBlockImgContainer}>
+              <img src={statInfoBlockImg} alt="" />
+            </div>
 
-        <button className={s.statInfoBlockBtn}>Перейти в бота</button>
-      </div>
+            <button className={s.statInfoBlockBtn}>Перейти в бота</button>
 
-      <div className={s.statInfoBlock}>
-        <div className={s.statInfoElement}>
-          <div className={s.statInfoElementTextBlock}>
-            <span>Онлайн</span>
-            <span>153</span>
           </div>
-        </div>
 
-        <div className={s.statInfoElement}>
-          <div className={s.statInfoElementTextBlock}>
-            <span>Количество пользователей</span>
-            <span>35 312</span>
-          </div>
-        </div>
-      </div>
+          <div className={s.statInfoBlock}>
+            <div className={s.statInfoElement}>
+              <div className={s.statInfoElementTextBlock}>
+                <span>Онлайн</span>
+                <span>153</span>
+              </div>
+            </div>
 
-      <div className={s.statInfoBlock}>
-        <div className={s.statInfoElement}>
-          <div className={s.statInfoElementTextBlock}>
-            <span>Количество сообщений</span>
-            <span>35 682 195</span>
+            <div className={s.statInfoElement}>
+              <div className={s.statInfoElementTextBlock}>
+                <span>Количество пользователей</span>
+                <span>35 312</span>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className={s.statInfoElement}>
-          <div className={s.statInfoElementTextBlock}>
-            <span>Количество дней с основания</span>
-            <span>162</span>
+          <div className={s.statInfoBlock}>
+            <div className={s.statInfoElement}>
+              <div className={s.statInfoElementTextBlock}>
+                <span>Количество сообщений</span>
+                <span>35 682 195</span>
+              </div>
+            </div>
+
+            <div className={s.statInfoElement}>
+              <div className={s.statInfoElementTextBlock}>
+                <span>Количество дней с основания</span>
+                <span>162</span>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </>
+      ) : (
+        <>
+          <div className={s.statInfoBlockSmall}>
+            <div className={s.statInfoElementSmall}>
+              <span>Количество пользователей</span>
+              <span>35 312</span>
+            </div>
+
+            <div className={s.statInfoElementSmall}>
+              <span>Количество сообщений</span>
+              <span>35 682 195</span>
+            </div>
+
+            <div className={s.statInfoElementSmall}>
+              <span>Количество дней с основания</span>
+              <span>162</span>
+            </div>
+
+            <div className={s.statInfoElementSmall}>
+              <span>Онлайн</span>
+              <span>153</span>
+            </div>
+          </div>
+        </>
+      )}
+    </div >
   );
 };
 
