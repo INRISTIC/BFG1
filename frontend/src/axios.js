23d724fd3a0893fe13a922583b212bb1d291b946
m769m <m_769_m@vk.com>
2023-01-01 20:28:30 +0700

    01_01_2023v8

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const inctance = axios.create({
-  baseURL: `${window.location.protocol}//${window.location.hostname}:3005`
+  baseURL: `http://195.201.7.129:3005`
 });
 
 export default inctance;
